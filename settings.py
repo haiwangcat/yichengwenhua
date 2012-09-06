ac000fc5e4e26d49129aba827cd3ef4b0c8c17f4
Cong Hou <hou_cong@gatech.edu>
2012-09-06 14:34:52 -0400

    Init

---
+++ settings.py
@@ -0,0 +1,83 @@
+# Initialize App Engine and import the default settings (DB backend, etc.).
+# If you want to use a different backend you have to remove all occurences
+# of "djangoappengine" from this file.
+from djangoappengine.settings_base import *
+
+import os
+
+# Activate django-dbindexer for the default database
+DATABASES['native'] = DATABASES['default']
+DATABASES['default'] = {'ENGINE': 'dbindexer', 'TARGET': 'native'}
+AUTOLOAD_SITECONF = 'indexes'
+
+SECRET_KEY = '=r-$b*8hglm+858&9t043hlm6-&6-3d3vfc4((7yd0dbrakhvi'
+
+
+from os import path
+
+MEDIA_ROOT = path.join(path.abspath(path.dirname(__file__)), 'media')
+STATIC_ROOT = path.join(path.abspath(path.dirname(__file__)), 'static')
+
+MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+
+INSTALLED_APPS = (
+#    'django.contrib.admin',
+    'django.contrib.contenttypes',
+    'django.contrib.auth',
+    'django.contrib.sessions',
+    'djangotoolbox',
+    'autoload',
+    'dbindexer',
+    'cms',
+    'menus',
+    'mptt',
+    'south',
+    'cms.plugins.text',
+    'cms.plugins.picture',
+    'cms.plugins.link',
+    'cms.plugins.file',
+    'cms.plugins.snippet',
+    'cms.plugins.googlemap',
+    'sekizai',
+
+    # djangoappengine should come last, so it can override a few manage.py commands
+    'djangoappengine',
+)
+
+MIDDLEWARE_CLASSES = (
+    # This loads the index definitions, so it has to come first
+    'autoload.middleware.AutoloadMiddleware',
+
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'cms.middleware.page.CurrentPageMiddleware',
+    'cms.middleware.user.CurrentUserMiddleware',
+    'cms.middleware.toolbar.ToolbarMiddleware',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.request',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'cms.context_processors.media',
+    'sekizai.context_processors.sekizai',
+)
+
+CMS_TEMPLATES = (
+    ('example.html', 'Example Template'),
+    ('index.html', 'Index Template'),
+    ('opening.html', 'Opening Template'),
+)
+
+# This test runner captures stdout and associates tracebacks with their
+# corresponding output. Helps a lot with print-debugging.
+TEST_RUNNER = 'djangotoolbox.test.CapturingTestSuiteRunner'
+
+ADMIN_MEDIA_PREFIX = '/media/admin/'
+TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), 'templates'),)
+
+ROOT_URLCONF = 'urls'
