fef06b93a2af793a86033dbb2b84f0d2e0d2b1cf
Cong Hou <hou_cong@gatech.edu>
2012-09-13 22:00:43 -0400

    Added local dir

--- local/settings.py
+++ local/settings.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*-
+# Django settings for gallery project.
+
 import os
 
 gettext = lambda s: s
@@ -14,15 +15,13 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-LANGUAGES = [('en', 'en')]
-DEFAULT_LANGUAGE = 0
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(PROJECT_DIR, 'mycms.db'),
     }
 }
+
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -31,11 +30,11 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Asia/Shanghai'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-cn'
 
 SITE_ID = 1
 
@@ -47,11 +46,7 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/"
-
 from os import path
-
 MEDIA_ROOT = path.join(path.abspath(path.dirname(__file__)), 'media')
 STATIC_ROOT = path.join(path.abspath(path.dirname(__file__)), 'static')
 
@@ -63,18 +58,34 @@ MEDIA_URL = '/media/'
 #STATIC_URL = '/static/'
 STATIC_URL = 'http://127.0.0.1:8000/static/'
 
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+# URL prefix for admin static files -- CSS, JavaScript and images.
+# Make sure to use a trailing slash.
+# Examples: "http://foo.com/static/admin/", "/static/admin/".
+#ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '0r6%7gip5tmez*vygfv+u14h@4lbt^8e2^26o#5_f_#b7%cm)u'
+SECRET_KEY = 'j8r8wglw%(k0@91qt6&yig0*($38q1rst(4n-1taw$ruyskf4*'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -99,16 +110,16 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 )
 
 CMS_TEMPLATES = (
-    ('example.html', 'Example Template'),
     ('index.html', 'Index Template'),
     ('opening.html', 'Opening Template'),
+    ('heritage.html', 'Heritage Template'),
+    ('heritage-index.html', 'Heritage Index Template'),
 )
 
 ROOT_URLCONF = 'urls'
 
-TEMPLATE_DIRS = (
-    os.path.join(PROJECT_DIR, 'templates'),
-)
+DIRNAME = os.path.abspath(os.path.dirname(__file__))
+TEMPLATE_DIRS = (os.path.join(DIRNAME, 'templates/'),)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -116,8 +127,12 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    'django.contrib.admin',
     'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'photologue', 
+    'yichengwenhua',
+    #'gallery.mygallery',
+    #'gallery.master_gallery',
     'cms',
     'menus',
     'mptt',
@@ -129,5 +144,36 @@ INSTALLED_APPS = (
     'cms.plugins.snippet',
     'cms.plugins.googlemap',
     'sekizai',
+    # Uncomment the next line to enable the admin:
+    # Uncomment the next line to enable admin documentation:
+    # 'django.contrib.admindocs',
 )
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
+CMS_PLUGIN_PROCESSORS = (
+    'yichengwenhua.cms_plugin_processors.process_heritage_text',
+)
+
 
--- local/urls.py
+++ local/urls.py
@@ -1,12 +1,32 @@
+# -*- coding: utf-8 -*-
 from django.conf.urls.defaults import *
 from django.contrib import admin
 from django.conf import settings
+from photologue.models import Gallery
 
 handler500 = 'djangotoolbox.errorviews.server_error'
 
 admin.autodiscover()
 
+
+from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render_to_response
+
+def getImages(request, slug):
+    if request.is_ajax():
+        galleries = Gallery.objects.filter(title_slug = slug)
+        if galleries.count() == 0:
+            return HttpResponse('<p>很抱歉，目前没有相关图片。</p>')
+        return render_to_response('show-images.html', {'album': galleries[0]})
+    else:
+        return HttpResponse('<p>很抱歉，目前没有相关图片。</p>')
+    
+    
+
 urlpatterns = patterns('',
+    (r'.*/(?P<slug>[\-\d\w]+)/get_images/?$', getImages),
+    #url(r'.*/(?P<slug>[\-\d\w]+)/get_images$', 'django.views.generic.list_detail.object_detail', {'slug_field': 'title_slug', 'queryset': Gallery.objects.filter(is_public=True), 'template_name': 'show-images.html'}),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('cms.urls')), 
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
